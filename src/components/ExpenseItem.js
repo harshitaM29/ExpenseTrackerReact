4477c022919e40471472254a03e8a36712fc0a35
harshitaM29 <harshita.mundhe29@gmail.com>
2023-05-22 10:03:56 +0530

    styling custom HTML component and using JS in JSX code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle  = 'Car Insurance';
+    const expenseAmount = 294.67;
+    const locationOfExpenditure = 'Mumbai';
+    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+              <h2>{expenseTitle}</h2>
+              <h2>{locationOfExpenditure}</h2>
+              <div className='expense-item__price'>Rs.{expenseAmount}</div>
+            </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
