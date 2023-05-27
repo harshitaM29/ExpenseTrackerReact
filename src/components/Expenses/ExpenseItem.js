3b76494f3afee384ecc32abb18ee3b68422c2cff
harshitaM29 <harshita.mundhe29@gmail.com>
2023-05-27 17:21:11 +0530

    added code for removing expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,18 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    
+    const clickHandler = (e) => {
+        var display = document.getElementsByClassName('expense-item');
+        var len = display.length;
+        for (var i = 0; i < len; i++) {
+           display[0].remove();
+        }
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} location={props.location} amount={props.amount}   />
-   
+            <button onClick={clickHandler}>Delete</button>
     </Card>
     );
 }
